253fcd36de50a57ca94d0117a00f233d1b9d03a9
nguyendm77 <nguyendm77@gmail.com>
2021-01-24 20:58:56 -0800

    saved

--- Exercise2/03_13e/script.js
+++ Exercise2/03_13e/script.js
@@ -6,14 +6,14 @@
 import Backpack from "./Backpack.js";
 
 const everydayPack = new Backpack(
-  "Everyday Backpack",
-  30,
-  "grey",
-  15,
-  26,
-  26,
-  false,
-  "December 5, 2018 15:00:00 PST"
+	"Everyday Backpack",
+	30,
+	"grey",
+	15,
+	26,
+	26,
+	false,
+	"December 5, 2018 15:00:00 PST"
 );
 
 console.log("The everydayPack object:", everydayPack);
