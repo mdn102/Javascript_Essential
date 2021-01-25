ae742c875a71095f20de8fb21e1df509161f44c4
nguyendm77 <nguyendm77@gmail.com>
2021-01-24 18:18:23 -0800

    updated code

--- Exercise2/03_03/script.js
+++ Exercise2/03_03/script.js
@@ -3,20 +3,20 @@
  */
 
 const backpack = {
-  name: "Everyday Backpack",
-  volume: 30,
-  color: "grey",
-  pocketNum: 15,
-  strapLength: {
-    left: 26,
-    right: 26,
-  },
-  lidOpen: false,
-  toggleLid: function (lidStatus) {
-    this.lidOpen = lidStatus;
-  },
-  newStrapLength: function (lengthLeft, lengthRight) {
-    this.strapLength.left = lengthLeft;
-    this.strapLength.right = lengthRight;
-  },
+	name: "Everyday Backpack",
+	volume: 30,
+	color: "grey",
+	pocketNum: 15,
+	strapLength: {
+		left: 26,
+		right: 26,
+	},
+	lidOpen: false,
+	toggleLid: function (lidStatus) {
+		this.lidOpen = lidStatus;
+	},
+	newStrapLength: function (lengthLeft, lengthRight) {
+		this.strapLength.left = lengthLeft;
+		this.strapLength.right = lengthRight;
+	},
 };
